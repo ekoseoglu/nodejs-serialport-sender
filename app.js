a1ebba4d935e1b21aa14d7f85c60cf1207a697d7
Esat Köseoğlu <ekoseoglu22@gmail.com>
2024-02-24 11:36:55 +0300

    sender added string to hex converer

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 import { SerialPort } from 'serialport';
 
-const path = '/dev/tty.usbserial-0001';
+const path = '/dev/ttyUSB1';
 const baudRate = 9600;
 
 const serialport = new SerialPort({ path, baudRate });
 
-const hexString = '01a168656c6c6fffe1';
-const data = Buffer.from(hexString, 'hex');
+const myString = 'Hello, My name is john. What is your name?';
+const buffer = Buffer.from(myString);
+const hexString = buffer.toString('hex');
+
+const startDelimiter = '01a1';
+const stopDelimiter = 'ffe1';
+
+const concatHexString = `${startDelimiter}${hexString}${stopDelimiter}`;
+const data = Buffer.from(concatHexString, 'hex');
 
 serialport.on('open', () => {
   console.log('connected to serial port: ', path);
 
   setInterval(() => {
-    serialport.write('test', () => {
-      console.log('wrote hello');
+    serialport.write(data, () => {
+      console.log('send packet: ', data.toString('hex'));
     });
   }, 5000);
 });
-
